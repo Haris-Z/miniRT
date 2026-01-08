a7582b9bf79bc130961735da10d41aedeebaaaa3
Haris Zunic <zunic.haris42@gmail.com>
2026-01-08 16:34:48 +0100

    update: doxy comment templates for later

--- inc/rt.h
+++ inc/rt.h
@@ -1,68 +1,170 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rt.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 00:13:51 by hazunic           #+#    #+#             */
+/*   Updated: 2026/01/08 16:33:24 by hazunic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file	rt.h
+ * @brief	This file contains ... (add more info later)
+ * 
+ */
 
 #ifndef RT_H
 # define RT_H
 
 # define RT_WINDOW_NAME	"miniRT"
-# define SCREEN_WIDTH	800 // 1920 
-# define SCREEN_HEIGHT	600 // 1080 // full screen
+# define SCREEN_WIDTH	800
+# define SCREEN_HEIGHT	600
 
+# include <limits.h>
 # include <math.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h>
-
-//# include "../minilibx-linux/mlx.h"
+# include <X11/X.h>			// contatins keycodes
+# include <X11/keysymdef.h>
+# include <X11/keysym.h>
 # include "mlx.h"
-// # include "rt_keys.h"
+# include "rt_error.h"
+
 // # include "vec3.h"
 // # include "ray.h"
 // # include "color.h"
 // # include "scene.h"
-# include "parser.h"
+// # include "parser.h"
 
-/* add spp */
+/**
+ * @brief 
+ * 
+ */
 typedef struct s_img
 {
-	void	*img_ptr;
+	void	*ptr;
 	char	*addr;
-	int		bpp;
-	int		line_len;
-	int		endian;
-	int		w;
-	int		h;
+	int		bpp;		// set ?
+	int		line_len;	// set ?
+	int		endian;		// set ?
+	int		img_w;
+	int		img_h;
 }	t_img;
 
-/* add int flags and  */
-typedef struct s_app
+/**
+ * @brief 
+ * 
+ */
+typedef struct s_rt_mlx
 {
 	void	*mlx;
 	void	*win;
 	t_img	img;
 	int		w;
 	int		h;
+	// int screendim[2]
 	int		should_quit;
-	t_scene	scene;
+	//t_scene	scene;
 	// err_context
 	// state
 	// flag
-}	t_app;
+}	t_rt_mlx;
 
 /* app */
-int		rt_app_init(t_app *app, int w, int h, const char *title);
-int		rt_app_run(t_app *app);
-void	rt_app_destroy(t_app *app);
+
+/**
+ * @brief 
+ * 
+ * @param rt 
+ * @param w 
+ * @param h 
+ * @param title 
+ * @return int 
+ */
+int		rt_init(t_rt_mlx *rt, int w, int h, const char *title);
+
+/**
+ * @brief 
+ * 
+ * @param rt 
+ * @return int 
+ */
+int		rt_run(t_rt_mlx *rt);
+
+/**
+ * @brief 
+ * 
+ * @param rt 
+ */
+void	rt_destroy(t_rt_mlx *rt);
 
 /* mlx image wrapper */
-int		rt_img_create(t_app *app, t_img *img, int w, int h);
-void	rt_img_destroy(t_app *app, t_img *img);
+
+/**
+ * @brief 
+ * 
+ * @param rt 
+ * @param img 
+ * @param w 
+ * @param h 
+ * @return int 
+ */
+int		rt_img_create(t_rt_mlx *rt, t_img *img, int w, int h);
+
+/**
+ * @brief 
+ * 
+ * @param rt 
+ * @param img 
+ */
+void	rt_img_destroy(t_rt_mlx *rt, t_img *img);
+
+/**
+ * @brief 
+ * 
+ * @param img 
+ * @param x 
+ * @param y 
+ * @param color 
+ */
 void	rt_img_put_pixel(t_img *img, int x, int y, int color);
 
-/* events */
-int		rt_on_keypress(int keycode, void *param);
-int		rt_on_destroy(void *param);
-int		rt_on_expose(void *param);
-int		rt_on_loop(void *param);
+/* events/hooks */
 
-int		rt_error(const char *msg);
+/**
+ * @brief 
+ * 
+ * @param keycode 
+ * @param param 
+ * @return int 
+ */
+int		rt_on_keypress(int keycode, void *param);
+
+/**
+ * @brief 
+ * 
+ * @param param 
+ * @return int 
+ */
+int		rt_on_destroy(void *param);
+
+/**
+ * @brief 
+ * 
+ * @param param 
+ * @return int 
+ */
+int		rt_on_expose(void *param);
+
+/**
+ * @brief 
+ * 
+ * @param param 
+ * @return int 
+ */
+int		rt_on_loop(void *param);
 
 #endif
