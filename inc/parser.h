77049f4afc9718cc7a86586fe7997d42ec4fa6a3
Haris Zunic <zunic.haris42@gmail.com>
2026-01-08 16:25:05 +0100

    added: parser.h

--- inc/parser.h
+++ inc/parser.h
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rt_parser.h                                        :+:      :+:    :+:   */
+/*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 17:49:47 by hazunic           #+#    #+#             */
-/*   Updated: 2026/01/06 17:49:51 by hazunic          ###   ########.fr       */
+/*   Updated: 2026/01/08 16:22:25 by hazunic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-# include "scene.h"
+// # include "scene.h"
 
-int	parse_rt_file(const char *path, t_world *w);
+typedef struct s_scene t_scene;
+
+/**
+ * @brief 
+ * 
+ * @param path 
+ * @param s 
+ * @return int 
+ */
+int	parse_rt_file(const char *path, t_scene *s);
 
 #endif
