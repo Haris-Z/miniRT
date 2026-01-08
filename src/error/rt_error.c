e29fa1aed94ae0becd4d733a7273c805de899318
Haris Zunic <zunic.haris42@gmail.com>
2026-01-08 16:47:43 +0100

    update: rt_error.c

--- src/error/rt_error.c
+++ src/error/rt_error.c
@@ -6,7 +6,7 @@
 /*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 15:09:01 by hazunic           #+#    #+#             */
-/*   Updated: 2026/01/07 18:40:10 by hazunic          ###   ########.fr       */
+/*   Updated: 2026/01/08 16:45:52 by hazunic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,44 @@
 
 static const char	*rt_strerror(t_eflag e);
 
-int	rt_log_error(int code, const char *msg, int i)
+/**
+ * @brief 
+ * 
+ * @param code 
+ * @param msg 
+ * @param i 
+ * @return int
+ * @todo split function - check for msg NULL arg NULL, param arg not in use currently
+ */
+int	rt_log_error(int code, const char *msg, int i, char *arg)
 {
+	(void)arg;
 	ft_putendl_fd(E_MSG, 2);
 	if (code > E_PARSE_BASE && code < E_PARSE_END)
 	{
 		ft_putstr_fd("[PARSE] ", 2);
-		ft_putstr_fd("Line: : ", 2);
-		ft_putnbr_fd(i, 2);
+		if (i >= 0)
+		{
+			ft_putstr_fd("Line ", 2);
+			ft_putnbr_fd(i, 2);
+			ft_putstr_fd(": ", 2);
+		}
 		ft_putendl_fd((char *)rt_strerror(code), 2);
+		//return (code);
 	}
 	else if (code > E_RT_BASE && code < E_RT_END)
 	{
 		ft_putstr_fd("[RT]", 2);
-		ft_putstr_fd((char *)rt_strerror(code), 2);
-		ft_putchar_fd('\n', 2);
+		ft_putendl_fd((char *)rt_strerror(code), 2);
 	}
 	else if (code == E_SYS)
 	{
-		ft_putstr_fd("[SYS]", 2);
-		ft_putstr_fd(": ", 2);
-		ft_putstr_fd(strerror(errno), 2);
-		ft_putchar_fd('\n', 2);
+		ft_putstr_fd("[SYS]: ", 2);
+		ft_putendl_fd(strerror(errno), 2);
+		// ft_putstr_fd(strerror(errno), 2);
+		// ft_putstr_fd(" | ", 2);
+		// ft_putnbr_fd(errno, 2);
+		// ft_putendl_fd("", 2);
 	}
 	else
 		ft_putendl_fd((char *)msg, 2);
@@ -47,13 +63,17 @@ int	rt_log_error(int code, const char *msg, int i)
 }
 
 /**
- * @brief Get error message string
+ * @brief 
+ * 
+ * @param e 
+ * @return const char* 
  */
 static const char	*rt_strerror(t_eflag e)
 {
 	const char	*msgs[E_COUNT] = {
 	[E_OK] = "Success",
-	[E_USAGE] = ERR_USAGE
+	[E_USAGE] = ERR_USAGE,
+	[E_RT_MLX_INIT] = MSG_RT_MLX_INIT
 	};
 
 	if ((int)e < 0 || e >= E_COUNT)
