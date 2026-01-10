1cea22573aa07d96407d1e493bd7b1c2846147d4
Haris Zunic <zunic.haris42@gmail.com>
2026-01-10 12:52:16 +0100

    update: error logging

--- src/error/rt_error.c
+++ src/error/rt_error.c
@@ -6,7 +6,7 @@
 /*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 15:09:01 by hazunic           #+#    #+#             */
-/*   Updated: 2026/01/09 20:33:19 by hazunic          ###   ########.fr       */
+/*   Updated: 2026/01/10 12:50:42 by hazunic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <string.h>
 
 static const char	*rt_strerror(t_eflag e);
+static void			rt_parse_err(int i, char *msg, char *arg);
 
 int	rt_error_msg(const char *msg)
 {
@@ -38,19 +39,11 @@ int	rt_error_msg(const char *msg)
 int	rt_log_error(int code, const char *msg, int i, char *arg)
 {
 	(void)arg;
+	(void)msg;
 	ft_putendl_fd(E_MSG, 2);
 	if (code > E_PARSE_BASE && code < E_PARSE_END)
 	{
-		ft_putstr_fd("[PARSE] ", 2);
-		if (i >= 0)
-		{
-			ft_putstr_fd("Line ", 2);
-			ft_putnbr_fd(i, 2);
-			ft_putstr_fd(": ", 2);
-		}
-		ft_putendl_fd((char *)rt_strerror(code), 2);
-		if (arg)
-			ft_putstr_fd(arg, 2);
+		rt_parse_err(i, (char *)rt_strerror(code), arg);
 		return (code);
 	}
 	else if (code > E_RT_BASE && code < E_RT_END)
@@ -70,6 +63,56 @@ int	rt_log_error(int code, const char *msg, int i, char *arg)
 	else
 		ft_putendl_fd((char *)msg, 2);
 	return (1);
+}
+// int	rt_log_error(int code, const char *msg, int i, char *arg)
+// {
+// 	(void)arg;
+// 	ft_putendl_fd(E_MSG, 2);
+// 	if (code > E_PARSE_BASE && code < E_PARSE_END)
+// 	{
+// 		ft_putstr_fd("[PARSE] ", 2);
+// 		if (i >= 0)
+// 		{
+// 			ft_putstr_fd("Line ", 2);
+// 			ft_putnbr_fd(i, 2);
+// 			ft_putstr_fd(": ", 2);
+// 		}
+// 		ft_putendl_fd((char *)rt_strerror(code), 2);
+// 		if (arg)
+// 			ft_putstr_fd(arg, 2);
+// 		return (code);
+// 	}
+// 	else if (code > E_RT_BASE && code < E_RT_END)
+// 	{
+// 		ft_putstr_fd("[RT]", 2);
+// 		ft_putendl_fd((char *)rt_strerror(code), 2);
+// 	}
+// 	else if (code == E_SYS)
+// 	{
+// 		ft_putstr_fd("[SYS]: ", 2);
+// 		ft_putendl_fd(strerror(errno), 2);
+// 		// ft_putstr_fd(strerror(errno), 2);
+// 		// ft_putstr_fd(" | ", 2);
+// 		// ft_putnbr_fd(errno, 2);
+// 		// ft_putendl_fd("", 2);
+// 	}
+// 	else
+// 		ft_putendl_fd((char *)msg, 2);
+// 	return (1);
+// }
+
+static void rt_parse_err(int i, char *msg, char *arg)
+{
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd("[PARSE]: ", 2);
+	ft_putstr_fd("Line ", 2);
+	ft_putnbr_fd(i, 2);
+	ft_putstr_fd(": ", 2);
+	if (msg)
+		ft_putstr_fd(msg, 2);
+	if (arg)
+		ft_putstr_fd(arg, 2);
+	ft_putstr_fd("\n", 2);
 }
 
 /**
