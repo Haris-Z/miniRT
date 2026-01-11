2f8e9a9e905ba634772891fb678f274fd0caaf5e
Haris Zunic <zunic.haris42@gmail.com>
2026-01-11 10:18:33 +0100

    update: added comments

--- src/parse/parse_cam.c
+++ src/parse/parse_cam.c
@@ -6,7 +6,7 @@
 /*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 11:02:44 by hazunic           #+#    #+#             */
-/*   Updated: 2026/01/09 14:43:26 by hazunic          ###   ########.fr       */
+/*   Updated: 2026/01/11 10:17:36 by hazunic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,27 @@ int	parse_unit_vec(t_vec3 v)
 {
 	double	len;
 
-	if (!in_range(v.x, -1.0, 1.0) || !in_range(v.y, -1.0, 1.0)
+	if (!in_range(v.x, -1.0, 1.0)
+		|| !in_range(v.y, -1.0, 1.0)
 		|| !in_range(v.z, -1.0, 1.0))
-		return(rt_error_msg("Unit vector components out of range [-1..1] for Camera orientation (C)"));
+		return (rt_error_msg("Unit vector components out of range [-1..1] for Camera orientation (C)"));
 	len = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 	if (len < 1e-12)
-		return(rt_error_msg("Unit vector is zero fo for Camera orientation (C)"));
+		return (rt_error_msg("Unit vector is zero fo for Camera orientation (C)"));
 	if (fabs(len - 1.0) > 1e-3)
-		return(rt_error_msg("Vector must be normalized for Camera orientation (C)"));
+		return (rt_error_msg("Vector must be normalized for Camera orientation (C)"));
 	return (0);
 }
 
 int	parse_fov(const char *tok, double *out)
 {
-	int	f;
+	int	fov;
 
-	if (parse_int(tok, &f))
-		return(rt_error_msg("Camera (C): invalid FOV"));
-	if (f < 0.0 || f > 180.0)
-		return(rt_error_msg("Camera (C): FOV out of range [0..180]"));
-	*out = f;
+	if (parse_int(tok, &fov)) // E_PARSE_BAD_FOV
+		return(rt_error_msg("Camera (C): invalid FOV")); // E_PARSE_BAD_FLOAT // E_PARSE_BAD_INT
+	if (fov < 0 || fov > 180)
+		return(rt_error_msg("Camera (C): FOV out of range [0..180]")); // E_PARSE_RANGE_FOV
+	*out = fov;
 	return (0);
 }
 
@@ -65,7 +66,7 @@ int	parse_camera(t_scene *s, char **t)
 	if (parse_unit_vec(cam.dir) != 0)
 		return (1);
 	if (parse_fov(t[3], &cam.fov_deg) != 0)
-		return (1);
+		return (E_PARSE_BAD_FOV);
 	if (scene_set_camera(s, cam) != 0)
 		return(rt_error_msg("Duplicate camera (C)"));
 	return (0);
