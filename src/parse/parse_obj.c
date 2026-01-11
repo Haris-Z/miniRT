5e43eea98a95a841e350ad30c5ebfb6db103cacc
Haris Zunic <zunic.haris42@gmail.com>
2026-01-11 10:21:04 +0100

    fix: added diameter to sphere

--- src/parse/parse_obj.c
+++ src/parse/parse_obj.c
@@ -6,7 +6,7 @@
 /*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 21:11:22 by hazunic           #+#    #+#             */
-/*   Updated: 2026/01/09 18:26:14 by hazunic          ###   ########.fr       */
+/*   Updated: 2026/01/11 10:20:09 by hazunic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 #include "parser.h"
 #include "rt_error.h"
 
-// id	center	diameter/radius		color
-// sp	0,0,20				20		255,0,0
+// id	center	diameter		color
+// sp	0,0,20	12.6			255,0,0
 int	parse_sphere(t_scene *s, char **t)
 {
 	t_sphere	sp;
-	double		d;
 
 	if (!t[1] || !t[2] || !t[3] || t[4])
 		return(rt_error_msg("Sphere (sp): expected 'sp x,y,z diameter R,G,B'"));
 	if (parse_vec3(t[1], &sp.center) != 0)
 		return(rt_error_msg("Invalid vector for Sphere (sp) center"));
-	if (parse_double(t[2], &d) != 0 || d <= 0.0)
+	if (parse_double(t[2], &sp.diameter) != 0 || sp.diameter <= 0.0)
 		return(rt_error_msg("Sphere (sp): diameter must be > 0"));
-	sp.radius = d * 0.5;
+	sp.radius = sp.diameter * 0.5;
 	if (parse_color_vec(t[3], &sp.color) != 0 )
 		return(rt_error_msg("Sphere (sp): rgb invalid/out of range"));
 	if (scene_add_sphere(s, sp) != 0)
