1c384969038baeffe9088cbb35b5562344ff05af
Haris Zunic <zunic.haris42@gmail.com>
2026-01-12 08:36:43 +0100

    added: intersect.h

---
+++ inc/intersect.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   intersect.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/11 12:59:24 by hazunic           #+#    #+#             */
+/*   Updated: 2026/01/11 13:17:03 by hazunic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INTERSECT_H
+# define INTERSECT_H
+
+# include "scene.h"
+# include "hit.h"
+
+// hit_sphere
+// hit_plane
+// hit_cylinder
+// hit_objects
+
+#endif // INTERSECT_H
