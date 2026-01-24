467acb93ae93b77b673ded2aa5c3a961baddfa5b
Haris Zunic <zunic.haris42@gmail.com>
2026-01-24 16:59:09 +0100

    update: save_to_ppm function now doesnt overwrite new images hack

--- src/write_to_ppm/save_ppm.c
+++ src/write_to_ppm/save_ppm.c
@@ -6,17 +6,17 @@
 /*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 18:37:39 by hazunic           #+#    #+#             */
-/*   Updated: 2026/01/24 11:52:09 by hazunic          ###   ########.fr       */
+/*   Updated: 2026/01/24 16:55:38 by hazunic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <sys/time.h>
+#include <math.h>
 #include <unistd.h>
 #include <stdio.h>
 #include "libft.h"
 #include "mrt.h"
-
-static int	write_to_ppm(int fd, const void *buf, size_t n);
 
 // ppm format
 // P6\n
@@ -25,6 +25,16 @@ static int	write_to_ppm(int fd, const void *buf, size_t n);
 // P6
 // <width> <height>
 // 255
+static void	write_ppm_header(int width, int height, int fd)
+{
+	ft_putendl_fd("P6", fd);
+	ft_putnbr_fd(width, fd);
+	ft_putstr_fd(" ", fd);
+	ft_putnbr_fd(height, fd);
+	ft_putstr_fd("\n255\n", fd);
+}
+
+static int	write_to_ppm(int fd, const void *buf, size_t n);
 
 /// @brief save image to ppm
 /// - might need a check for endiannes - in school is 0
@@ -41,18 +51,36 @@ int	save_to_ppm(const char *path, const t_rt_img *img)
 	unsigned char		rgb[3];
 
 	if (!path || !img || !img->addr || img->img_w <= 0 || img->img_h <= 0)
-		return (1);											//-rw-r--r--
-	fd = open(path, O_CREAT | O_TRUNC | O_RDWR , 0644);
+		return (1);
+	// factor out to extra func
+	char				buf[512];
+	char				*start;
+	char				*usec;
+	struct timeval		tv;
+	
+	buf[0] = '\0';
+	ft_strlcat(buf, "saved_images/", sizeof buf);
+	start = ft_strrchr(path, '/') + 1;
+	ft_strlcat(buf, start,  ft_strlen((char *)buf) + ft_strlen(start) - ft_strlen(".rt") + 1);
+	ft_strlcat(buf, "_", sizeof buf);
+	gettimeofday(&tv, NULL);
+	usec = ft_itoa((int)tv.tv_usec / 1000);
+	if (!usec)
+	{
+		perror("usec");
+		return (1);
+	}
+	ft_strlcat(buf, usec, sizeof buf);
+	free(usec);
+	ft_strlcat(buf, ".ppm", sizeof buf);
+
+	fd = open((const char *)buf, O_CREAT | O_TRUNC | O_RDWR , 0644); //-rw-r--r--
 	if (fd < 0)
 	{
 		perror("open");
 		return (1);
 	}
-	ft_putendl_fd("P6", fd);
-	ft_putnbr_fd(img->img_w, fd);
-	ft_putstr_fd(" ", fd);
-	ft_putnbr_fd(img->img_h, fd);
-	ft_putstr_fd("\n255\n", fd);
+	write_ppm_header(img->img_w, img->img_h, fd);
 	bpp = img->bpp / 8;
 	y = -1;
 	while (++y < img->img_h)
@@ -73,6 +101,8 @@ int	save_to_ppm(const char *path, const t_rt_img *img)
 		}
 	}
 	close(fd);
+	ft_putstr_fd("created: ", 1);
+	ft_putendl_fd((char *)buf, 1);
 	return (0);
 }
 
