3cfad74ca7ecfb2f19036032b2b1b6a5a008f7cf
Haris Zunic <hazunic@c1r3p4.42vienna.com>
2026-02-06 16:34:16 +0100

    update: printV and printM

--- src/debug/prints.c
+++ src/debug/prints.c
@@ -6,7 +6,7 @@
 /*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 19:25:53 by hazunic           #+#    #+#             */
-/*   Updated: 2026/02/05 19:55:55 by hazunic          ###   ########.fr       */
+/*   Updated: 2026/02/06 16:27:20 by hazunic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,4 +70,16 @@ void	print_vector(char *name, t_vec3 v)
 		printf("%s: ", name);
 	printf("(%.2f, %.2f, %.2f)", v.x, v.y, v.z);
 	printf("\n");
+}
+
+void	printV(t_vec3 a)
+{
+	printf("x: %f y: %f z: %f\n", a.x,a.y,a.z);
+}
+
+void	printM(t_mat3 a)
+{
+	printf("	%f	%f	%f\n", a.colx.x, a.coly.x, a.colz.x);
+	printf("	%f	%f	%f\n", a.colx.y, a.coly.y, a.colz.y);
+	printf("	%f	%f	%f\n", a.colx.z, a.coly.z, a.colz.z);
 }
