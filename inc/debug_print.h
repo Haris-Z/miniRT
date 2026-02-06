c8b025a257e7427d49543027a407ef416de38dfa
Haris Zunic <hazunic@c1r3p4.42vienna.com>
2026-02-06 18:04:58 +0100

    update: functions prototypes

--- inc/debug_print.h
+++ inc/debug_print.h
@@ -6,17 +6,19 @@
 /*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 19:22:35 by hazunic           #+#    #+#             */
-/*   Updated: 2026/02/06 16:27:42 by hazunic          ###   ########.fr       */
+/*   Updated: 2026/02/06 18:02:46 by hazunic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEBUG_PRINT_H
 # define DEBUG_PRINT_H
 
-
 # include "mrt.h"
 
+// debug prints
+
 void	print_scene_info(t_scene scene, char *file);
+void	print_vector(char *name, t_vec3 v);
 void	printV(t_vec3 a);
 void	printM(t_mat3 a);
 
