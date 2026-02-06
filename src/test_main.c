29980ebff36cc90246d443a02397703cafbf2674
Haris Zunic <hazunic@c1r3p4.42vienna.com>
2026-02-06 15:23:35 +0100

    update: main func with list-to-array

--- src/test_main.c
+++ src/test_main.c
@@ -6,7 +6,7 @@
 /*   By: hazunic <hazunic@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 11:53:53 by hazunic           #+#    #+#             */
-/*   Updated: 2026/02/05 19:28:47 by hazunic          ###   ########.fr       */
+/*   Updated: 2026/02/05 21:51:35 by hazunic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,44 +28,79 @@ void	rotTest()
 	printV(a);
 }
 
+
+/*
+**  ###############################
+**	MAIN WITH LIST-TO-ARRAY
+**  ###############################
+*/
+
 int	main(int argc, char **argv)
 {
 	t_rt_mlx	app;
 	t_scene		scene_info;
-
+	int			i;
+	int			j;
+	int			k;
+	t_obj		*objs;
+	int			objs_n;
+	
+	// check args
 	ft_bzero(&app, sizeof(app));
 	if (argc != 2)
 		return(rt_error_msg(MSG_USAGE));
+		
+	// init scene parse file
 	ft_bzero(&scene_info, sizeof(scene_info));
 	if(parse_file(argv[1], &scene_info) != 0)
 		return (1);
-
 	print_scene_info(scene_info, argv[1]);
+	
+	// list to array
+	if (scene_obj_array(&scene_info) != 0)
+		return (1);
+
+
+	// CHECK 
+	// int t = 0;
+	objs = scene_info.objs_arr;
+	objs_n = scene_info.objs_n;
+	// while (t < objs_n)
+	// {
+	// 	if (objs[t].type == OBJ_SPHERE)
+	// 	{
+	// 		printf("SPHERE[%d]: %.2f\n", t, objs[t].sphere.radius);
+	// 		print_vector("center", objs[t].sphere.center);
+	// 		print_vector("color", objs[t].sphere.color);
+	// 	}
+	// 	t++;
+	// }
+	// exit(1);
+
+	// init mlx stuff
 	if (rt_init(&app, RT_WINDOW_NAME) != 0)
 		return (1);
+
+	// init render stuff
 	app.cam = cam_init(scene_info, SCREEN_WIDTH, SCREEN_HEIGHT);
-	app.cam.items = &scene_info.objs;
 	if (!dir_vector_init(&app.cam))
 		return (0); // was a mallocerr
-	int	i = -1;
-	t_obj	*item;
-	///start = timer()
+	app.cam.items = &scene_info.objs_arr;
+	(*app.cam.items)->obj_total = objs_n;
+
 	// rotTest(app.cam);
+	i = -1;
 	while(++i < app.cam.pixels[1])
 	{
 		add_dir_vector_row(&app.cam);
-		item = *app.cam.items;
-		int j;
-		while(item)
-		{
-			update_ray_dist(&app, item);
-			item = item->next;
-		}
+		k = -1;
+		while(++k < scene_info.objs_n)
+			update_ray_dist(&app, &((*app.cam.items)[k]));
 		j = -1;
 		while(++j < app.cam.pixels[0])
 		{
 			if (app.cam.rays[j].closestitem)
-				app.img.addr[i * app.w +j] = color_to_mlx(compute_color(app,app.cam.rays[j], app.cam.items));
+				app.img.addr[i * app.w + j] = color_to_mlx(compute_color(app,app.cam.rays[j], app.cam.items));
 		}
 	}/// end = timer(
 	// printf(end-start);
@@ -78,3 +113,63 @@ int	main(int argc, char **argv)
 	scene_clear(&scene_info);
 	return (0);
 }
+
+/*
+**  ###############################
+**	PREVIOUS MAIN WITH LIST
+**  ###############################
+*/
+
+// int	main(int argc, char **argv)
+// {
+// 	t_rt_mlx	app;
+// 	t_scene		scene_info;
+
+// 	// check args
+// 	ft_bzero(&app, sizeof(app));
+// 	if (argc != 2)
+// 		return(rt_error_msg(MSG_USAGE));
+// 	// init scene parse file
+// 	ft_bzero(&scene_info, sizeof(scene_info));
+// 	if(parse_file(argv[1], &scene_info) != 0)
+// 		return (1);
+// 	print_scene_info(scene_info, argv[1]);
+
+	
+// 	if (rt_init(&app, RT_WINDOW_NAME) != 0)
+// 		return (1);
+// 	app.cam = cam_init(scene_info, SCREEN_WIDTH, SCREEN_HEIGHT);
+// 	app.cam.items = &scene_info.objs;
+// 	if (!dir_vector_init(&app.cam))
+// 		return (0); // was a mallocerr
+// 	int	i = -1;
+// 	t_obj	*item;
+// 	///start = timer()
+// 	// rotTest(app.cam);
+// 	while(++i < app.cam.pixels[1])
+// 	{
+// 		add_dir_vector_row(&app.cam);
+// 		item = *app.cam.items;
+// 		int j;
+// 		while(item)
+// 		{
+// 			update_ray_dist(&app, item);
+// 			item = item->next;
+// 		}
+// 		j = -1;
+// 		while(++j < app.cam.pixels[0])
+// 		{
+// 			if (app.cam.rays[j].closestitem)
+// 				app.img.addr[i * app.w +j] = color_to_mlx(compute_color(app,app.cam.rays[j], app.cam.items));
+// 		}
+// 	}/// end = timer(
+// 	// printf(end-start);
+// 	printf("DONE!!\n");
+// 	fflush(NULL);
+// 	mlx_put_image_to_window(app.mlx, app.win, app.img.ptr, 0, 0);
+// 	// save_to_ppm(argv[1], &app.img);
+// 	rt_run(&app);
+// 	rt_destroy(&app);
+// 	scene_clear(&scene_info);
+// 	return (0);
+// }
